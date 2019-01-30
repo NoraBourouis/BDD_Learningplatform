407cd40e5b86de56b7657f8429b09a433cac5317
hnassri <46187243+hnassri@users.noreply.github.com>
2019-01-30 22:21:00 +0100

    ajout coms

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 require 'faker'
-
+# On crée 10 cours
 10.times do
   lecture = Lecture.create!(title: Faker::GameOfThrones.house)
 end
-
+# On crée 10 leçons qu'on associe à un cours
 10.times do
   student = Student.create!(name: Faker::GameOfThrones.character, lecture_id: rand(1..11))
 end
